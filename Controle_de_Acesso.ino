f085b5c8f52b0c0f81b1cfd9763b39abff83040a
Frederik <63610483+FrederikSantAna@users.noreply.github.com>
2022-01-27 15:17:17 -0300

    Commit1

---
+++ Controle_de_Acesso.ino
@@ -0,0 +1,11 @@
+void setup() {
+  //Definindo duas saídas para as portas das salas 
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
