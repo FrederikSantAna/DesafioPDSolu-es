c7fa8a47be70798e31c6fee1bed8efa9942cc4e5
Frederik <63610483+FrederikSantAna@users.noreply.github.com>
2022-01-29 20:10:50 -0300

    Salvando Eventos

--- Controle_de_Acesso.ino
+++ Controle_de_Acesso.ino
@@ -1,5 +1,7 @@
 #include <EEPROM.h>
 #include "EEPROMString.h"
+
+
 enum {
   STATE_MENU, //0
   STATE_1,    //1
@@ -19,11 +21,11 @@ struct Usuario {
 };
 
 Usuario usuario[10] = {};
-String eventos[10];
+String eventos[10] = {};
 char state;
-int i, cadastroAtual = 0, usuarioAtual, ativaTempo = 0, buttonState = 0, eventoAtual, endereco = 0, enderecosSalvos = 0;
+int i, cadastroAtual = 0, usuarioAtual, ativaTempo = 0, buttonState = 0, eventoAtual, endereco = 0, enderecoEventos = 0, enderecosSalvos = 0;
 String dadoAtual;
-bool validacaoAcesso, contagemTempo, validacaoAdm;
+bool validacaoAcesso, contagemTempo, validacaoAdm, nomeIgual;
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
 
@@ -39,21 +41,27 @@ void setState(char newState) {
   validacaoAcesso = false;
   contagemTempo = false;
   validacaoAdm = false;
+  nomeIgual = false;
 }
 
 void CarregaInfoEEPROM() {
   //Reservando a posicao 0 e 1 da memoria para o numero do cadastro atual, um int consome 2 bytes
   cadastroAtual = EEPROM.read(0);
+  //Reservando a posicao 2 e 3 da memoria para o numero do evento atual
+  eventoAtual = EEPROM.read(2);
 
   String texto = readStringEEPROM(endereco);
+  String texto2 = readStringEEPROM(enderecoEventos);
 
-  //Serial.println("-----");
+  Serial.println("-----");
   //Serial.println("CarregaInfoEEPROM()");
   //Serial.println(cadastroAtual);
+  Serial.println(eventoAtual);
   //Serial.println(texto);
-  //Serial.println("-----");
+  Serial.println(texto2);
+  Serial.println("-----");
 
-  int inicioN, inicioS, inicioA, fimN, fimS, fimA, controle = 0, nUsuario = 0;
+  int inicioN, inicioS, inicioA, fimN, fimS, fimA, controle = 0, nUsuario = 0, inicioEvento, fimEvento;
   for (int i = 0; i < texto.length(); i++) {
     //Acresta dado na struct
     if (texto[i] == '/') {
@@ -87,6 +95,22 @@ void CarregaInfoEEPROM() {
       }
     }
   }
+
+  nUsuario = 0;
+
+  for (int i = 0; i < texto2.length(); i++) {
+    if (texto2[i] == '/') {
+      fimEvento = i;
+      eventos[nUsuario] = texto2.substring(inicioEvento, fimEvento);
+      nUsuario++;
+    }
+    if (texto2[i] == '!') {
+      inicioEvento = i + 1;
+    }
+  }
+
+  Serial.println(eventos[0]);
+  Serial.println(eventos[1]);
 }
 
 void setup() {
@@ -105,17 +129,22 @@ void setup() {
   contagemTempo = false;
   validacaoAdm = false;
 
-  //A linha abaixo serve para limpar o numero de cadastros da memoria
+  //A trecho abaixo serve para limpar a memoria
   /*
     cadastroAtual = 0;
     EEPROM.write(0,cadastroAtual);
+    eventoAtual = 0;
+    EEPROM.write(2,eventoAtual);
     for (int nL = 0; nL < 1000; nL++) {
       EEPROM.write(nL, 0);
     }
   */
-  //Reservando a posicao 20 em diante da memoria para as strings
+  //Reservando a posicao 20 em diante da memoria para as strings dos cadastros
   endereco = 20;
+  //Reservando a posicao 900 em diante da memoria para as strings dos eventos
+  enderecoEventos = 900;
 
+  //Chamando a função que carrega as informações da memoria
   CarregaInfoEEPROM();
 }
 
@@ -156,23 +185,50 @@ void loop() {
       break;
     case STATE_1: //Cadastro do usuário
       if (i == 0) {
+        Serial.println("Nome e Senha não deve conter os caracteres - e /");
         Serial.print("Nome:");
         i = 1;
       }
       if (Serial.available() > 0) {
         i++;
+        String texto = Serial.readString();
         if (i == 2) {
-          usuario[cadastroAtual].nome = Serial.readString();
-          Serial.println(usuario[cadastroAtual].nome);
-          Serial.print("Senha:");
+          Serial.println(texto);
+          for (int i = 0; i < cadastroAtual; i++) {
+            if (texto == usuario[i].nome) {
+                 nomeIgual = true;
+            }
+          }
+          if(nomeIgual) {
+            i--;
+            Serial.println("Esse nome já está cadastrado, tente novamente!");
+            Serial.print("Nome:");
+            nomeIgual = false;
+          }
+          else if ((texto.indexOf('-') != -1) || (texto.indexOf('/') != -1)) {
+            i--;
+            Serial.println("Não pode ser usado - e /, tente novamente!");
+            Serial.print("Nome:");
+          }
+          else {
+            usuario[cadastroAtual].nome = texto;
+            Serial.print("Senha:");
+          }
         }
         else if (i == 3) {
-          usuario[cadastroAtual].senha = Serial.readString();
-          Serial.println(usuario[cadastroAtual].senha);
-          Serial.print("Administrador(S ou N):");
+          Serial.println(texto);
+          if ((texto.indexOf('-') != -1) || (texto.indexOf('/') != -1)) {
+            i--;
+            Serial.println("Não pode ser usado - e /, tente novamente");
+            Serial.print("Senha:");
+          }
+          else {
+            usuario[cadastroAtual].senha = texto;
+            Serial.print("Administrador(S ou N):");
+          }
         }
         else if (i == 4) {
-          usuario[cadastroAtual].administrador = Serial.readString();
+          usuario[cadastroAtual].administrador = texto;
           Serial.println(usuario[cadastroAtual].administrador);
           if ((usuario[cadastroAtual].administrador == "S") || (usuario[cadastroAtual].administrador == "N")) {
             Serial.println("Usuario Cadastrado!");
@@ -182,35 +238,42 @@ void loop() {
             writeStringEEPROM(endereco, texto1 + texto2);
 
             cadastroAtual++;
-            //Salva na memoria o ultimo cadastro e seu endereco final
+            //Salva na memoria o ultimo cadastro
             EEPROM.write(0, cadastroAtual);
             setState(STATE_MENU);
           }
           else {
             i--;
-            Serial.println("Incorreto!");
-            Serial.print("Administrador(S ou N):");
+            Serial.println("Incorreto!Responda com S ou N");
+            Serial.print("Administrador:");
           }
         }
       }
       break;
     case STATE_2: //Listagem dos nomes dos usuários cadastrados
-      Serial.println("Usuarios Cadastrados:");
-
-      for (int i = 0; i < cadastroAtual; i++) {
-        Serial.println(usuario[i].nome);
-        //Serial.println(usuario[i].senha);
-        //Serial.println(usuario[i].administrador);
+      if (readStringEEPROM(endereco) == "") {
+        Serial.println("Nenhum Usuario Cadastrado!");
       }
-
+      else {
+        Serial.println("Usuarios Cadastrados:");
+        for (int i = 0; i < cadastroAtual; i++) {
+          Serial.println(usuario[i].nome);
+          Serial.println(usuario[i].senha);
+          Serial.println(usuario[i].administrador);
+        }
+      }
       setState(STATE_MENU);
       break;
     case STATE_3: //Listagem dos eventos
-      if (i == 0) {
+      if (readStringEEPROM(enderecoEventos) == "") {
+        Serial.println("Nenhum Evento Registrado!");
+        setState(STATE_MENU);
+      }
+      else if (i == 0) {
         Serial.print("Nome:");
         i = 1;
       }
-      if (Serial.available() > 0 && !validacaoAcesso) {
+      else if (Serial.available() > 0 && !validacaoAcesso) {
         dadoAtual = Serial.readString();
         for (int i = 0; i < 10; i++) {
           if (dadoAtual == usuario[i].nome) {
@@ -226,7 +289,7 @@ void loop() {
           i = 0;
         }
       }
-      if (Serial.available() > 0 && validacaoAcesso) {
+      else if (Serial.available() > 0 && validacaoAcesso) {
         dadoAtual = Serial.readString();
         if (dadoAtual == usuario[usuarioAtual].senha) {
           Serial.println(dadoAtual);
@@ -239,7 +302,8 @@ void loop() {
           Serial.print("Senha:");
         }
         if (validacaoAcesso && (usuario[usuarioAtual].administrador == "S")) {
-          for (int i = 0; i < 10; i++) {
+          Serial.println("Eventos:");
+          for (int i = 0; i < eventoAtual; i++) {
             Serial.println(eventos[i]);
             setState(STATE_MENU);
           }
@@ -260,13 +324,17 @@ void loop() {
 }
 
 void LiberaPorta(int numeroPorta, int buttonPorta) {
-  if (i == 0) {
+  if (readStringEEPROM(endereco) == "") {
+    Serial.println("Nenhum Usuario Cadastrado!");
+    setState(STATE_MENU);
+  }
+  else if (i == 0) {
     Serial.print("Nome:");
     i = 1;
   }
-  if (Serial.available() > 0 && !validacaoAcesso) {
+  else if (Serial.available() > 0 && !validacaoAcesso) {
     dadoAtual = Serial.readString();
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < cadastroAtual; i++) {
       if (dadoAtual == usuario[i].nome) {
         usuarioAtual = i;
         validacaoAcesso = true;
@@ -280,7 +348,7 @@ void LiberaPorta(int numeroPorta, int buttonPorta) {
       i = 0;
     }
   }
-  if (Serial.available() > 0 && validacaoAcesso) {
+  else if (Serial.available() > 0 && validacaoAcesso) {
     dadoAtual = Serial.readString();
     if (dadoAtual == usuario[usuarioAtual].senha) {
       Serial.println(dadoAtual);
@@ -288,8 +356,25 @@ void LiberaPorta(int numeroPorta, int buttonPorta) {
       digitalWrite(numeroPorta, HIGH);
       contagemTempo = true;
       previousMillis = currentMillis;
-      eventos[eventoAtual] = usuario[usuarioAtual].nome + " - " + usuario[usuarioAtual].senha + " - Acesso a porta 2";
+
+      String porta;
+      if (numeroPorta == 2) {
+        porta = "Porta 1";
+      } if (numeroPorta == 3) {
+        porta = "Porta 2";
+      }
+      eventos[eventoAtual] = usuario[usuarioAtual].nome + '-' + usuario[usuarioAtual].senha + '-' + porta;
+      String texto1 = readStringEEPROM(enderecoEventos);
+      String texto2 = '!' + eventos[eventoAtual] + '/';
+      writeStringEEPROM(enderecoEventos, texto1 + texto2);
+
+      //Serial.println(texto1);
+      //Serial.println(texto2);
+      //Serial.println(readStringEEPROM(enderecoEventos));
+
       eventoAtual++;
+      //Salva na memoria o ultimo evento
+      EEPROM.write(2, eventoAtual);
     }
     else {
       Serial.println(dadoAtual);
